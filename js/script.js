72c1b71bc13abe051825d6550110435b7909f652
Maciej Celuch <maciej.celuch@gmail.com>
2017-03-26 20:48:59 +0200

    mapSection

--- js/script.js
+++ js/script.js
@@ -8,21 +8,19 @@ function setWindowHeight() {
 };
 
 function setMiddleHeight(){
-    var halfWindowHeight = window.innerHeight/2 - 100;
-    var classElements = document.getElementsByClassName('middleWindowHeigh');
-    for( i=0 ; i<classElements.length; i++){
-        classElements[i].style.marginTop = halfWindowHeight + "px";
+    var halfWindowHeight = window.innerHeight/2 - 200;
+    var classElements2 = document.getElementsByClassName('middleWindowHeigh');
+    for( i=0 ; i<classElements2.length; i++){
+        classElements2[i].style.paddingTop = halfWindowHeight + "px";
     }
 }
 
 
 
-$(document).ready(function(){
-    setWindowHeight();
-    setMiddleHeight();
-});
 
 $(document).ready(function() {
+    setWindowHeight();
+    setMiddleHeight();
     $(window).resize(function() {
         setWindowHeight();
         setMiddleHeight();
